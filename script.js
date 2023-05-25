11b7af5f75baf6c890b03ef781eef627c57a9f70
winniewambui <winniebwambui7@gmail.com>
2023-05-25 09:51:58 +0300

    another commit

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 //top notch movies
 const topMovies = [
-   // {
-   //    "id" :1,
-   //    "title" :"The Great",
-   //    "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-   //  },
-//   {
-//      "id" :2,
-//      "title" :"The Great",
-//      "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-//  },
- {
-  "id" :3,
-  "title" :"The Great",
-  "image" :"https://images.unsplash.com/photo-1569254994521-ddbb54af5ae8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-},
-{
-  "id" :4,
-  "title" :"The Great",
-  "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-},
-{
-  "id" :5,
-  "title" :"The Great",
-  "image" :"https://images.unsplash.com/photo-1472017454701-20a80cfeaac5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8OTM0NzM5Nnx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
-},
-{
-  "id" :6,
-  "title" :"The Great",
-  "image" :"https://plus.unsplash.com/premium_photo-1669868116752-acd4bc7bc6a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-},
-{
-   "id" :7,
-   "title" :"The Great",
-   "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-}
-]
+       {
+          "id" :1,
+          "title" :"The Great",
+          "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+      },
+      {
+          "id" :2,
+          "title" :"The Great",
+          "image" :"https://images.unsplash.com/photo-1472017454701-20a80cfeaac5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8OTM0NzM5Nnx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
+       },
+       {
+          "id" :3,
+          "title" :"The Great",
+          "image" :"https://plus.unsplash.com/premium_photo-1669868116752-acd4bc7bc6a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+       },
+       {
+          "id" :4,
+          "title" :"The Great",
+          "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+       },
+       {
+           "id" :5,
+           "title" :"The Great",
+           "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+       }
+   ]
 
 const sectionTopMovies = document.querySelector('.section-items');
 
@@ -48,6 +38,50 @@ const section =topMovies.map((item) => {
 });
 
 sectionTopMovies.innerHTML =section.join("");
+
+
+//imageSlider IMAGES
+const imageSlider = document.querySelector(".section-items");
+// const firstImage = document.querySelector(".popular-movies-slideshow img");
+const slides = document.querySelectorAll(".section-items img")
+
+const previousBtn = document.querySelector(".prev-button");
+const nextBtn = document.querySelector(".next-button");
+let slide = 0;
+const totalSlides = slides.length;
+
+  // If it's the last slide, reset to 0, else +1
+nextBtn.addEventListener("click", () =>{
+   if (slide === totalSlides - 1) {
+     slide = 0;
+   }
+   else{
+      slide ++;
+   }
+   updateSlide();
+   // firstImage.style.display = "none";
+   // otherImages[position].style.display = "block";
+});
+ // If it's the first slide, set as the last slide, else -1
+  previousBtn.addEventListener("click", () =>{
+      if (slide === 0) {
+         slide = totalSlides - 1; // move to the last slide
+     } else {
+         // move back one
+         slide --;
+
+   };   
+   updateSlide();
+   // firstImage.style.display = "none";
+   // otherImages[position].style.display = "block";
+});
+function updateSlide() {
+   imageSlider.style.transform = `translateX(-${slide * 25}%)`;
+ }
+//  imageSlider.style.display = "flex";
+//  imageSlider.style.flexWrap = "wrap";
+
+
 
 //slideshow for popular movies
 const  popularMoviesData = [
@@ -118,38 +152,6 @@ const popularMovies = popularMoviesData.map((item) => {
 
 movies.innerHTML =popularMovies.join("");
 
-
-
-
-
-
-
-
-
-
-
-//DARK & LIGHT MODE
-const icon = document.querySelector(".theme-icon");
-const darkMode = document.querySelector(".moon");
-const lightMode = document.querySelector(".sun");
-
-icon.addEventListener('click', change);
-
-function change (){
-
-   document.body.classList.toggle('dark-mode');
-
-   if(document.body.classList.contains('dark-mode')){
-      darkMode.style.visiblity = 'invisible';
-      lightMode.style.visiblity = 'visible';
-   
-       } else {
-          darkMode.style.visiblity = 'visible';
-          lightMode.style.visiblity = 'invisible';
- 
- }
-}
-
 //CAROUSEL IMAGES
 const carousel = document.querySelector(".popular-movies-slideshow");
 // const firstImage = document.querySelector(".popular-movies-slideshow img");
@@ -204,6 +206,38 @@ function showHide (e){
       genreContents.style.visibility = "hidden";
     }
    }
+
+
+
+
+
+
+
+
+
+
+//DARK & LIGHT MODE
+const icon = document.querySelector(".theme-icon");
+const darkMode = document.querySelector(".moon");
+const lightMode = document.querySelector(".sun");
+
+icon.addEventListener('click', change);
+
+function change (){
+
+   document.body.classList.toggle('dark-mode');
+
+   if(document.body.classList.contains('dark-mode')){
+      darkMode.style.visiblity = 'invisible';
+      lightMode.style.visiblity = 'visible';
+   
+       } else {
+          darkMode.style.visiblity = 'visible';
+          lightMode.style.visiblity = 'invisible';
+ 
+ }
+}
+
 
 
 
