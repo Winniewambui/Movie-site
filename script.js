8d465c270ea765134e9661e8d38f591923e8c9fb
winniewambui <winniebwambui7@gmail.com>
2023-05-27 15:23:19 +0300

    resolved conflicts in the image sliders

--- script.js
+++ script.js
@@ -24,12 +24,24 @@ const topMovies = [
          "id" :5,
          "title" :"The Great",
          "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-     }
-      //  {
-      //      "id" :6,
-      //      "title" :"The Great",
-      //      "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
-      //  }
+     }, 
+     {
+      "id" :6,
+      "title" :"The Great",
+      "image" :"https://images.unsplash.com/photo-1472017454701-20a80cfeaac5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8OTM0NzM5Nnx8ZW58MHx8fHx8&auto=format&fit=crop&w=700&q=60"
+   },
+   {
+      "id" :7,
+      "title" :"The Great",
+      "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+  },
+  {
+   "id" :8,
+   "title" :"The Great",
+   "image" :"https://images.unsplash.com/photo-1598449356475-b9f71db7d847?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG9yaXpvbnRhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60"
+}
+
+     
    ];
 
 const sectionTopMovies = document.querySelector('.section-items');
@@ -37,7 +49,7 @@ const sectionTopMovies = document.querySelector('.section-items');
 const section =topMovies.map((item) => {
   return `
   <div class = "top-movies">
-   <img class="" src ="${item.image}" alt="${item.title}">
+   <img class="item" src ="${item.image}" alt="${item.title}">
    </div>
   `;
   
@@ -45,42 +57,52 @@ const section =topMovies.map((item) => {
 
 sectionTopMovies.innerHTML =section.join("");
 
-
 //imageSlider IMAGES
+
+const slides = sectionTopMovies.getElementsByClassName("item");
+
+function nextBtn(){
+   sectionTopMovies.append(slides[0]);
+}
+function previousBtn(){
+   sectionTopMovies.prepend(slides[slides.length - 1]);
+}
+
+
 // const imageSlider = document.querySelector(".section-items");
-const slides = document.querySelectorAll(".section-items img")
+// const sectionTopMovies = document.querySelector('.section-items');
+// const previousBtn = document.querySelector(".prev-button");
+// const nextBtn = document.querySelector(".next-button");
+// let slide = 0;
+// const totalSlides = slides.length;
 
-const previousBtn = document.querySelector(".prev-button");
-const nextBtn = document.querySelector(".next-button");
-let slide = 0;
-const totalSlides = slides.length;
 
-  // If it's the last slide, reset to 0, else +1
-nextBtn.addEventListener("click", () =>{
-   if (slide === totalSlides - 1) {
-     slide = 0;
-   }
-   else{
-      slide ++;
-   }
-   updateImage();
+// //   If it's the last slide, reset to 0, else +1
+// nextBtn.addEventListener("click", () =>{
+//    if (slide === totalSlides - 1) {
+//      slide = 0;
+//    }
+//    else{
+//       slide ++;
+//    }
+//    updateImage();
 
-});
- // If it's the first slide, set as the last slide, else -1
-  previousBtn.addEventListener("click", () =>{
-      if (slide === 0) {
-         slide = totalSlides - 1; // move to the last slide
-     } else {
-         // move back one
-         slide --;
+// });
+// //  If it's the first slide, set as the last slide, else -1
+//   previousBtn.addEventListener("click", () =>{
+//       if (slide === 0) {
+//          slide = totalSlides - 1; // move to the last slide
+//      } else {
+//          // move back one
+//          slide --;
 
-   };   
-   updateImage();
+//    };   
+//    updateImage();
 
-});
-function  updateImage() {
-   sectionTopMovies.style.transform = `translateX(-${slide * 100}%)`;
- }
+// });
+// function  updateImage() {
+//    sectionTopMovies.style.transform = `translateX(-${slide * 100}%)`;
+//  }
 
 
 
@@ -139,12 +161,12 @@ const  popularMoviesData = [
 
 ]
 
-const movies = document.querySelector('.popular-movies-slideshow');
+const carousel = document.querySelector('.popular-movies-slideshow');
 
 const popularMovies = popularMoviesData.map((item) => {
    return `
    <div class = "popular-movies">
-    <img class="" src ="${item.image}" alt="${item.title}">
+    <img class="img-item" src ="${item.image}" alt="${item.title}">
     <h2>${item.title}</h2>
     <p>${item.content}
 
@@ -152,59 +174,80 @@ const popularMovies = popularMoviesData.map((item) => {
    `
 });
 
-movies.innerHTML =popularMovies.join("");
+carousel.innerHTML =popularMovies.join("");
 
 //CAROUSEL IMAGES
-const carousel = document.querySelector(".popular-movies-slideshow");
-const carouselSlide = document.querySelectorAll(".popular-movies-slideshow img");
-const carouselParagraphs = document.querySelectorAll(".popular-movies p");
-const carouselHeadings = document.querySelectorAll(".popular-movies h2");
 
-const prevButton = document.querySelector("#carousel-prev-btn");
-const nextButton = document.querySelector("#carousel-next-btn");
-let position = 0;
-const numberOfSlides = carouselSlide.length;
+const carouselSlide = carousel.getElementsByClassName("img-item");
+
+function next(){
+   carousel.append(carouselSlide[0]);
+}
+function prev(){
+   carousel.prepend(carouselSlide[carouselSlide.length - 1]);
+}
+
+// const carousel = document.querySelector(".popular-movies-slideshow");
+// const carouselParagraphs = document.querySelectorAll(".popular-movies p");
+// const carouselHeadings = document.querySelectorAll(".popular-movies h2");
+
+// const prevButton = document.querySelector("#carousel-prev-btn");
+// const nextButton = document.querySelector("#carousel-next-btn");
+// let position = 0;
+// const numberOfSlides = carouselSlide.length;
 
 // Hide all images except the first one
-for (let i = 4; i < carouselSlide.length; i++) {
-   carouselSlide[i].style.display = "none";
-   carouselParagraphs[i].style.display = "none";
-   carouselHeadings[i].style.display = "none";
- }
+// for (let i = 4; i < carouselSlide.length; i++) {
+//    carouselSlide[i].style.display = "none";
+//    carouselParagraphs[i].style.display = "none";
+//    carouselHeadings[i].style.display = "none";
+//  }
 
-  // If it's the last slide, reset to 0, else +1
-nextButton.addEventListener("click", () =>{
-   if (position === numberOfSlides - 1) {
-      position = 0;
-   }
-   else{
-      position ++;
-   }; 
-   updateSlide();
 
-   carouselSlide[position].style.display = "block";
-   carouselParagraphs[position].style.display = "block";
-   carouselHeadings[position].style.display = "block";
+// // Set display property of carousel container to flex and fixed height
+// carousel.style.display = "flex";
+// carousel.style.height = "300px"; // Adjust the height as needed
 
-});
- // If it's the first slide, set as the last slide, else -1
-   prevButton.addEventListener("click", () =>{
-      if (position === 0) {
-         position = numberOfSlides - 1; // move to the last slide
-     } else {
-         // move back one
-         position --;
+// // Adjust the width of each slide to occupy the available space evenly
+// carouselSlide.forEach((slide) => {
+//    slide.style.maxWidth = "300px";
+//    slide.style.maxHeight = "100%";
+//    slide.style.objectFit = "contain";
+// });
 
-   };   
-   updateSlide();
+//   // If it's the last slide, reset to 0, else +1
+// nextButton.addEventListener("click", () =>{
+//    if (position === numberOfSlides - 1) {
+//       position = 0;
+//    }
+//    else{
+//       position ++;
+//    }; 
+//    updateSlide();
 
-   carouselSlide[position].style.display = "block";
-   carouselParagraphs[position].style.display = "block";
-   carouselHeadings[position].style.display = "block";
-});
-function updateSlide() {
-   carousel.style.transform = `translateX(-${position * 25}%)`;
- }
+//    carouselSlide[position].style.display = "block";
+//    carouselParagraphs[position].style.display = "block";
+//    carouselHeadings[position].style.display = "block";
+
+// });
+//  // If it's the first slide, set as the last slide, else -1
+//    prevButton.addEventListener("click", () =>{
+//       if (position === 0) {
+//          position = numberOfSlides - 1; // move to the last slide
+//      } else {
+//          // move back one
+//          position --;
+
+//    };   
+//    updateSlide();
+
+//    carouselSlide[position].style.display = "block";
+//    carouselParagraphs[position].style.display = "block";
+//    carouselHeadings[position].style.display = "block";
+// });
+// function updateSlide() {
+//    carousel.style.transform = `translateX(-${position * 25}%)`;
+//  }
 
 
 //dropdown section
